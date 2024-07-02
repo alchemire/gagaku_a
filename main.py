24c9b73322779f4a451d35ab31959cf15c80cd97
alchemire <wannataraxia@gmail.com>
2024-07-03 08:18:20 +0900

    forth commit

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ folder_path =  "music"
 mp3_files = glob.glob(os.path.join(folder_path, '*.mp3'))
 
 data = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
+    [0, 1, 2, 3, 4, 5, 6],
+    [7, 8, 9, 10 ,11 ,12 ,13],
+    [14, 15, 16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25, 26, 27],
+    [28, 29, 30, 31, 32, 33, 34],
+    [35, 36, 37, 38, 39, 40, 41],
+    [42, 43, 44, 45, 46, 47, 48],
 ]
 
 df = pd.DataFrame(data)
 
 with st.form("my-form"):
-    row = st.slider("ピコ[テンポ遅め]← →[テンポ早め]", -2, 2, 0)
-    column = st.slider("パコ[複雑、内省的]← →[単純、愉快]", -2, 2, 0)
+    row = st.slider("ピコ[テンポ遅め]← →[テンポ早め]", -3, 3, 0)
+    column = st.slider("パコ[複雑、内省的]← →[単純、愉快]", -3, 3, 0)
 
     submitted = st.form_submit_button("ピ！")
 if submitted: 
     st.audio(mp3_files[(df.iloc[row, column])], format="audio/mpeg", autoplay=True)
 
-st.audio("music/01 You Know What I Mean.mp3", format="audio/mpeg", autoplay=True)
-st.audio(mp3_files[df.iloc[2, 0]], format="audio/mpeg", autoplay=True)
 
 st.write("2024年10月14日 周人＆純果")
 
